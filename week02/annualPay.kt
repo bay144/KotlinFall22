b04ecc48ef22398d2c1cedd0694f696562340a7c
Ana <98979108+bay144@users.noreply.github.com>
2022-09-05 22:38:19 -0500

    Create annualPay.kt

---
+++ week02/annualPay.kt
@@ -0,0 +1,6 @@
+fun main(){
+  val name: String = "Ana"
+  var age: Int = 21
+  val annualPay: Double = 200000.00
+  print("My name is $name, my age is $age and I hope to earn $$annualPay per year")
+}
